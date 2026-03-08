e687f55c3dcbf86b883feae5be5715127f8dbd78
rdsystemframework <rd.system.framework@gmail.com>
2026-03-08 04:15:10 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,37 @@
 
+<script>
+
+fetch("bikes.csv")
+.then(response => response.text())
+.then(data => {
+
+let rows = data.split("\n");
+let gallery = document.getElementById("bike-gallery");
+
+rows.slice(1).forEach(row => {
+
+let cols = row.split(",");
+
+let name = cols[0];
+let price = cols[1];
+let image = cols[2];
+let link = cols[3];
+
+let bike = document.createElement("div");
+bike.className = "bike";
+
+bike.innerHTML = `
+<img src="${image}">
+<h3>${name}</h3>
+<p>${price}</p>
+<a href="${link}" target="_blank">Details</a>
+<a href="#probefahrt">Probefahrt</a>
+`;
+
+gallery.appendChild(bike);
+
+});
+
+});
+
+</script>
